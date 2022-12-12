16c1f4ff9457232fe4118eb8dcd9efbdfb8c6292
Koedesu <marco.valadez@uttn.mx>
2022-12-12 12:39:15 -0600

    Ya 30 ejercicios asu madre

---
+++ ejercicio30_1.php
@@ -0,0 +1,15 @@
+<?php
+
+session_start();
+
+if( isset($_SESSION["usuario"])){
+    
+    echo "Usuario: ".$_SESSION["usuario"]." </br> Estatus: ".$_SESSION["status"];
+
+}
+else{
+
+    echo "No hay sesion iniciada.";
+
+}
+?>
---
+++ ejercicio31.php
@@ -0,0 +1,39 @@
+<?php
+
+$txtNombre="";
+if($_POST){
+
+    $txtNombre= (isset($_POST['txtNombre']))?$_POST['txtNombre']:"";
+
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulario</title>
+</head>
+<body>
+    
+    <strong> Hola </strong>: <?php
+
+    echo $txtNombre;
+
+
+    ?>
+
+    <form action="ejercicio31.php" method="post">
+
+        <input type="text" name="txtNombre" id="">
+
+        <input type="submit" value="Enviar informacion">
+
+    </form>
+
+
+</body>
+</html>
