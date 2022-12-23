b20ad8064816d295a9c6762ff00f8a5f4ec746c8
Koedesu <marco.valadez@uttn.mx>
2022-12-23 15:45:55 -0600

    c me olvido este jejeje

--- ejercicio31.php
+++ ejercicio31.php
@@ -1,11 +1,22 @@
 <?php
 
 $txtNombre="";
+$rdgLenguaje="";
+
+$chkphp="";
+$chkhtml="";
+$chkcss="";
+
 if($_POST){
 
     $txtNombre= (isset($_POST['txtNombre']))?$_POST['txtNombre']:"";
+    $rdgLenguaje= (isset($_POST['lenguaje']))?$_POST['lenguaje']:"";
 
+    $chkphp= (isset($_POST['chkphp'])=="si")?"checked":"";
+    $chkhtml= (isset($_POST['chkhtml'])=="si")?"checked":"";
+    $chkcss= (isset($_POST['chkcss'])=="si")?"checked":"";
 
+    //print_r($rdgLenguaje);
 }
 
 ?>
@@ -23,7 +34,9 @@ if($_POST){
     <?php if($_POST){ ?>
 
         <strong> Hola </strong>: <?php echo $txtNombre;  ?>
-    
+        <br/>
+        <strong> Tu lenguaje es:  </strong>
+        <?php echo $rdgLenguaje; ?>
     
     <?php  }  ?>  
 
@@ -33,12 +46,17 @@ if($_POST){
         Nombre:</br>
         <input value="<?php echo $txtNombre; ?>" type="text" name="txtNombre" id="">
         </br>
-        Te gustá?:</br>
-        </br>php
-        </br>html
-        </br>css
+        Te gusta?:</br>
+        <br/>php: <input type="radio" <?php echo ($rdgLenguaje=="php")?"checked":""; ?> name="lenguaje" value="php" id=""> <br/>
+        <br/>html: <input type="radio" <?php echo ($rdgLenguaje=="html")?"checked":""; ?> name="lenguaje" value="html" id=""> <br/>
+        <br/>css: <input type="radio" <?php echo ($rdgLenguaje=="css")?"checked":""; ?> name="lenguaje" value="css" id=""> <br/>
 
-        <input type="submit" value="Enviar informacion">
+        Estas aprendiendo... <br/>
+        <br/> php: <input type="checkbox" <?php echo $chkphp; ?> name="chkphp" value="si" id="">
+        <br/> html: <input type="checkbox" <?php  echo $chkhtml; ?> name="chkhtml" alue="si" id="">
+        <br/> css: <input type="checkbox" <?php echo $chkcss; ?> name="chkcss" value="si" id="">
+
+        <br/> <input type="submit" value="Enviar informacion">
 
     </form>
 
